fc53be46072804bc8e62c2eb443ce970e416d267
unknown <stephencullen88@yahoo.ie>
2024-08-20 23:08:58 +0100

    deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://StephenC808.github.io',
+  base: '/front-end-project',
   integrations: [svelte()]
 });
